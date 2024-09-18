c567ef561fcc507c608a94f847567688ad75f52c
Jason-2k <329577273@qq.com>
2024-09-18 15:02:20 +0800

    V 1.0

---
+++ readme.md
@@ -0,0 +1,13 @@
+#  YOLOv8-POS
+YOLOv8-POS is a deep learning model based on the YOLOv8 architecture dedicated to coal rock image detection and classification. This project integrates the latest algorithms and techniques to achieve high accuracy and efficiency in coal recognition.
+## Installation
+### Install dependencies
+Before you start using YOLOv8-POS, you need to install the required dependencies.
+```bash
+pip install ultralytics
+```
+## Training, validation and testing
+Refer to [YOLOv8 Docs](https://docs.ultralytics.com) for more information on how to train, validate and test the model.
+
+## Licence
+This project is under the AGPL-3.0 licence. See the LICENSE file for more information.
---
+++ ultralytics/nn/modules/convolution/PConv.py
@@ -0,0 +1,96 @@
+import torch
+import torch.nn as nn
+from ultralytics.nn.modules import Conv
+
+
+class PConv(nn.Module):
+    # https://arxiv.org/abs/2303.03667
+    def __init__(self, dim, ouc, n_div=4, forward='split_cat'):
+        super().__init__()
+        self.dim_conv3 = dim // n_div
+        self.dim_untouched = dim - self.dim_conv3
+        self.partial_conv3 = nn.Conv2d(self.dim_conv3, self.dim_conv3, 3, 1, 1, bias=False)
+        self.conv = Conv(dim, ouc, k=1)
+
+        if forward == 'slicing':
+            self.forward = self.forward_slicing
+        elif forward == 'split_cat':
+            self.forward = self.forward_split_cat
+        else:
+            raise NotImplementedError
+
+    def forward_slicing(self, x):
+        # only for inference
+        x = x.clone()  # !!! Keep the original input intact for the residual connection later
+        x[:, :self.dim_conv3, :, :] = self.partial_conv3(x[:, :self.dim_conv3, :, :])
+        x = self.conv(x)
+        return x
+
+    def forward_split_cat(self, x):
+        # for training/inference
+        x1, x2 = torch.split(x, [self.dim_conv3, self.dim_untouched], dim=1)
+        x1 = self.partial_conv3(x1)
+        x = torch.cat((x1, x2), 1)
+        x = self.conv(x)
+        return x
+
+
+class PConvBottleneck(nn.Module):
+    """Standard bottleneck."""
+
+    def __init__(self, c1, c2, shortcut=True, g=1, k=(3, 3), e=0.5):
+        """Initializes a bottleneck module with given input/output channels, shortcut option, group, kernels, and
+        expansion.
+        """
+        super().__init__()
+        c_ = int(c2 * e)  # hidden channels
+        # self.cv1 = Conv(c1, c_, k[0], 1)
+        # self.cv2 = Conv(c_, c2, k[1], 1, g=g)
+
+        self.cv1 = PConv(c1, c_)
+        self.cv2 = PConv(c_, c2)
+
+        self.add = shortcut and c1 == c2
+
+    def forward(self, x):
+        """'forward()' applies the YOLO FPN to input data."""
+        return x + self.cv2(self.cv1(x)) if self.add else self.cv2(self.cv1(x))
+
+
+class C2f_PConv(nn.Module):
+    """Faster Implementation of CSP Bottleneck with 2 convolutions."""
+
+    def __init__(self, c1, c2, n=1, shortcut=False, g=1, e=0.5):
+        """Initialize CSP bottleneck layer with two convolutions with arguments ch_in, ch_out, number, shortcut, groups,
+        expansion.
+        """
+        super().__init__()
+        self.c = int(c2 * e)  # hidden channels
+        self.cv1 = Conv(c1, 2 * self.c, 1, 1)
+        self.cv2 = Conv((2 + n) * self.c, c2, 1)  # optional act=FReLU(c2)
+        self.m = nn.ModuleList(
+            PConvBottleneck(self.c, self.c, shortcut, g, k=((3, 3), (3, 3)), e=1.0) for _ in range(n))
+
+    def forward(self, x):
+        """Forward pass through C2f layer."""
+        y = list(self.cv1(x).chunk(2, 1))
+        y.extend(m(y[-1]) for m in self.m)
+        return self.cv2(torch.cat(y, 1))
+
+    def forward_split(self, x):
+        """Forward pass using split() instead of chunk()."""
+        y = list(self.cv1(x).split((self.c, self.c), 1))
+        y.extend(m(y[-1]) for m in self.m)
+        return self.cv2(torch.cat(y, 1))
+
+
+if __name__ == "__main__":
+    # Generating Sample image
+    image_size = (1, 64, 224, 224)
+    image = torch.rand(*image_size)
+
+    # Model
+    model = C2f_PConv(64, 128)
+
+    out = model(image)
+    print(out.size())
